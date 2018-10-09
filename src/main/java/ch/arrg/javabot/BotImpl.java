7cf04b7ae51b97ee3108936ea4ccab5430c92fc5
ThomasGirard <arrg.ch@gmail.com>
2018-10-09 19:10:53 +0100

    Bot will now try to identify with nick server on connect

--- src/main/java/ch/arrg/javabot/BotImpl.java
+++ src/main/java/ch/arrg/javabot/BotImpl.java
@@ -23,71 +23,82 @@ import ch.arrg.javabot.util.Logging;
 // starting with "!")
 
 public class BotImpl extends PircBot implements Bot {
-
+	
 	private static final String ENCODING = "utf-8";
-
+	
 	private BotLogic logic = new BotLogic();
-
+	
 	private boolean isPaused = false;
-
+	
 	private List<String> ignoredUsers = Lists.newArrayList(Const.strArray("bot.ignoredUsers"));
-
+	
 	public BotImpl() throws Exception {
 		Logging.log("Building bot " + Const.BOT_NAME);
 		setName(Const.BOT_NAME);
 		setLogin(Const.BOT_NAME);
 		setEncoding(ENCODING);
 	}
-
+	
 	public void start() throws NickAlreadyInUseException, IOException, IrcException {
 		Logging.log("Starting bot on " + Const.SERVER_URL + ":" + Const.SERVER_PORT);
 		connect(Const.SERVER_URL, Const.SERVER_PORT);
 	}
-
+	
 	@Override
 	protected void onConnect() {
 		super.onConnect();
-		Logging.log("Bot connected, joining " + Const.str("channels"));
+		Logging.log("Bot connected. Identifying...");
+		identify(Const.str("bot.identify.password"));
+
+		Logging.log("Joining " + Const.str("channels"));
 		String[] chans = Const.strArray("channels");
 		for(String chan : chans) {
 			joinChannel(chan);
 		}
 	}
-
+	
 	@Override
 	protected void onPrivateMessage(String sender, String login, String hostname, String message) {
 		// TODO hack : this will make all replies go the private conv, but it'd
 		// be better if the context was privmsg aware.
 		String channel = sender;
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, message);
-
+		
 		// DatabaseLogServiceProvider.get().logEvent(LogEvent.MESSAGE, ctx);
-
+		
 		if(ignoredUsers.contains(sender)) {
 			return;
 		}
-
+		
 		logic.onMessage(ctx);
 	}
-
+	
 	@Override
 	protected void onMessage(String channel, String sender, String login, String hostname, String message) {
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, message);
-
+		
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.MESSAGE, ctx);
-
+		
 		if(ignoredUsers.contains(sender)) {
 			return;
 		}
-
+		
 		logic.onMessage(ctx);
 	}
-
+	
 	@Override
 	protected void onJoin(String channel, String sender, String login, String hostname) {
+		if(sender.equals(Const.BOT_NAME)) {
+			onSelfJoin(channel);
+		}
+		
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, null);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.JOIN, ctx);
 		logic.onJoin(ctx);
+	}
+	
+	private void onSelfJoin(String channel) {
+		// Do nothing
 	}
 
 	@Override
@@ -95,65 +106,65 @@ public class BotImpl extends PircBot implements Bot {
 		BotContext ctx = new BotContext(this, Const.CHANNEL, sender, login, hostname, reason);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.QUIT, ctx);
 	}
-
+	
 	@Override
 	protected void onPart(String channel, String sender, String login, String hostname) {
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, "part");
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.PART, ctx);
 	}
-
+	
 	@Override
 	protected void onAction(String sender, String login, String hostname, String channel, String action) {
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, action);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.JOIN, ctx);
 	}
-
+	
 	@Override
 	protected void onTopic(String channel, String topic, String setBy, long date, boolean changed) {
 		// TODO logging topic : hostname and co ?
 		BotContext ctx = new BotContext(this, channel, setBy, setBy, setBy, topic);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.TOPIC, ctx);
 	}
-
+	
 	@Override
 	protected void onNickChange(String oldNick, String login, String hostname, String newNick) {
 		BotContext ctx = new BotContext(this, Const.CHANNEL, oldNick, login, hostname, newNick);
-
+		
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.NICK, ctx);
 	}
-
+	
 	@Override
 	public void sendMsg(String target, String message) {
 		sendMessage(target, message);
 	}
-
+	
 	@Override
 	public UserData getUserData(String user) {
 		return logic.getUserData(user);
 	}
-
+	
 	@Override
 	public void quit() {
 		Logging.log("Quitting");
 		quitServer(Const.QUIT_MESSAGE);
 		System.exit(1);
 	}
-
+	
 	@Override
 	public void adminPause() {
 		isPaused = true;
 	}
-
+	
 	@Override
 	public void adminUnpause() {
 		isPaused = false;
 	}
-
+	
 	@Override
 	public boolean isPaused() {
 		return isPaused;
 	}
-
+	
 	@Override
 	public Boolean toggleHandler(String handlerName) {
 		return logic.toggleHandler(handlerName);
--- src/main/java/ch/arrg/javabot/BotLogic.java
+++ src/main/java/ch/arrg/javabot/BotLogic.java
@@ -23,21 +23,22 @@ import ch.arrg.javabot.util.Logging;
 // TODO automatic summary of links ?
 // TODO images to ascii art
 // TODO random tweets
+// TODO new kind of Handler that can actively (not reactively) interact (e.g. for NuclearThrone)
 
 public class BotLogic {
-	
+
 	private Map<String, CommandHandler> handlers = new TreeMap<>();
 	private Set<CommandHandler> disabled = new HashSet<>();
 	private Map<String, IrcEventHandler> eventHandlers = new TreeMap<>();
-	
+
 	private final UserDb userDb;
-	
+
 	public BotLogic() throws Exception {
 		String userDbFile = Const.DATA_FILE;
 		Logging.log("Loading UserDb from " + userDbFile);
 		userDb = DataStoreUtils.fromFile(userDbFile);
 		DataStoreUtils.saveOnQuit(userDbFile, userDb);
-		
+
 		String handlersFile = Const.str("handlers.file");
 		List<String> classNames = FileUtils.readLines(new File(handlersFile));
 		Logging.log("Instantiating " + classNames.size() + " handlers from " + handlersFile + ".");
@@ -45,14 +46,14 @@ public class BotLogic {
 			instantiateHandler(className);
 		}
 	}
-	
+
 	private void instantiateHandler(String className) {
 		try {
 			Class<?> clazz = Class.forName(className);
 			Object newInstance = clazz.newInstance();
 			CommandHandler commandHandler = (CommandHandler) newInstance;
 			addHandler(commandHandler);
-			
+
 		} catch (ClassNotFoundException e) {
 			Logging.log("Couldn't not instantiate " + className + ": class not found.");
 			Logging.logException(e);
@@ -64,50 +65,50 @@ public class BotLogic {
 			Logging.logException(e);
 		}
 	}
-	
+
 	private void addHandler(CommandHandler h) {
 		handlers.put(h.getName(), h);
-		
+
 		if(h instanceof IrcEventHandler) {
 			eventHandlers.put(h.getName(), (IrcEventHandler) h);
 		}
 	}
-	
+
 	protected void onMessage(BotContext ctx) {
 		if(ctx.isPaused()) {
 			onMessagePauseMode(ctx);
 			return;
 		}
-		
+
 		String message = ctx.message.trim();
-		
+
 		if(message.startsWith(Const.str("bot.ignoredPattern"))) {
 			return;
 		}
-		
+
 		CommandMatcher matcher = CommandMatcher.make("+help");
 		if(matcher.matches(message)) {
 			onHelp(ctx, matcher.nextWord());
 			return;
 		}
-		
+
 		for(CommandHandler handler : handlers.values()) {
 			try {
 				if(disabled.contains(handler))
 					continue;
-				
+
 				handler.handle(ctx);
 			} catch (Exception e) {
 				Logging.logException(e);
 			}
 		}
 	}
-	
+
 	protected void onMessagePauseMode(BotContext ctx) {
 		for(CommandHandler handler : handlers.values()) {
 			if(!(handler instanceof AdminHandler))
 				continue;
-			
+
 			try {
 				handler.handle(ctx);
 			} catch (Exception e) {
@@ -115,11 +116,11 @@ public class BotLogic {
 			}
 		}
 	}
-	
+
 	public void onJoin(BotContext ctx) {
 		if(ctx.isPaused())
 			return;
-		
+
 		for(IrcEventHandler handler : eventHandlers.values()) {
 			try {
 				handler.onJoin(ctx.sender, ctx);
@@ -128,13 +129,13 @@ public class BotLogic {
 			}
 		}
 	}
-	
+
 	private void onHelp(BotContext ctx, String topic) {
-		
+
 		if(handlers.containsKey(topic)) {
 			handlers.get(topic).help(ctx);
 		} else {
-			
+
 			ctx.reply("Here are known handlers: ");
 			StringBuilder sb = new StringBuilder();
 			for(CommandHandler handler : handlers.values()) {
@@ -144,16 +145,16 @@ public class BotLogic {
 			ctx.reply("Send +help <handler> to get more info on a particular one.");
 		}
 	}
-	
+
 	public UserData getUserData(String user) {
 		return userDb.getOrCreateUserData(user);
 	}
-	
+
 	public Boolean toggleHandler(String handlerName) {
 		CommandHandler handler = handlers.get(handlerName);
 		if(handler == null)
 			return null;
-		
+
 		if(disabled.contains(handler)) {
 			disabled.remove(handler);
 			return true;
@@ -162,5 +163,5 @@ public class BotLogic {
 			return false;
 		}
 	}
-	
+
 }
